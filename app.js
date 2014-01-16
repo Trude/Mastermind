9ce6e3541b4b01206326e280260fa8adb356b151
emagnus <westerga@gmail.com>
2014-01-16 17:32:16 +0100

    Begynt på telling av riktige farger.

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 
-
 // lag instans
 var app = express();
 
@@ -19,11 +18,21 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
+	checkForCorrectColors(req.body);
 	res.render('mastermind', {title: 'Mastermind', guesses: guesses});
 });
 
 var checkForCorrectColors = function(guess) {
-
+	var correct = 0;
+	var almostCorrect = 0;
+	for(var colorGuessed in guess) {
+		for(var colorCorrect in fasit) {
+			if(colorGuessed==colorCorrect) {
+				almostCorrect++;
+			}
+		}
+	}
+	guesses.push({guess: guess, correct: correct, almostCorrect: almostCorrect});
 }
 
 app.listen(3000);
