347d871c262ea4b265a7f991bcce567f41429238
emagnus <westerga@gmail.com>
2014-01-16 17:17:55 +0100

    Lister opp tidligere gjettinger. Trenger ikke styling.

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 // definer at vi bruker jade
 app.set('view engine', 'jade');
+app.use(express.bodyParser());
+
+var guesses = [{guess: {color0: 'Red', color1: 'Green', color2: 'Green', color3: 'Yellow'}, correct:1, almostCorrect:1}];
+var fasit = {color0: 'Red', color1: 'Red', color2: 'Blue', color3: 'Green'};
 
 app.get('/', function(req, res) {
-	res.render('mastermind', {title: 'Mastermind'});
+	res.render('mastermind', {title: 'Mastermind', guesses: guesses});
 });
+
+app.post('/', function(req, res) {
+	res.render('mastermind', {title: 'Mastermind', guesses: guesses});
+});
+
+var checkForCorrectColors = function(guess) {
+
+}
 
 app.listen(3000);
 console.log('Server listening on port 3000');
